38ce3693fdc87b1b6b59f8749389193757726b15
KunjBapodariya <90176954+KunjBapodariya@users.noreply.github.com>
2024-03-06 16:35:51 +0530

    Add files via upload

---
+++ Composition/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Motor
+{
+    public void Start()
+    {
+        Console.WriteLine("Motor started");
+    }
+}
+
+class Vehicle
+{
+    private Motor motor;
+    public Vehicle()
+    {
+        motor = new Motor();
+    }
+
+    public void StartVehicle()
+    {
+        motor.Start();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vehicle vehicle = new Vehicle();
+        vehicle.StartVehicle();
+    }
+}
